5879d20c10ae3c6481c0709b4fd2372817155b4b
Mohammed Refat <mohammedrefat53@outlook.com>
2023-12-16 20:18:23 +0200

    task 2- selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,27 +7,28 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j = 0;
-    int temp, place = 0;
+	size_t i, j = 0;
+	int temp, place = 0;
 
-    for (i = 0; i < size; i++)
-    {
-        int min = array[i];
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < min )
-            {
-                min = array[j];
-                place = j;
-            }
-        }
-        
-        if (min != array[i])
-        {
-            temp = array[place];
-            array[place] = array[i];
-            array[i] = temp;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		int min = array[i];
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < min)
+			{
+				min = array[j];
+				place = j;
+			}
+		}
+
+		if (min != array[i])
+		{
+			temp = array[place];
+			array[place] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
+	}
 }
