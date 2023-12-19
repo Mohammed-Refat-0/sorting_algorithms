feebaa184bfe02fcb777d3abd1aad0738f2825a2
Mohammed Refat <mohammedrefat53@outlook.com>
2023-12-19 23:41:00 +0200

    ans

---
+++ 102-counting_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ *counting_sort -  sorts an array of integers in ascending order
+  using Counting sort
+ * @array: arry to sort
+ * @size: size of array
+ */
+
+void counting_sort(int *array, size_t size)
+{
+    return;
+}
